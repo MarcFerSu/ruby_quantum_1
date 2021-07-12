381927fd89e731774297528b442dac3dfa70a0f6
Marcelo.Fernandez <Marcelo.Fernandez@studentambassadors.com>
2021-07-12 17:23:39 -0500

    clases ruby

--- Evaluaciones/Examen final/examenfinal.rb
+++ Evaluaciones/Examen final/examenfinal.rb
@@ -1,34 +1,30 @@
 class Proceso
-  def analyze_string(string)
-    i = 0
-    string.each_char do
-      puts "---- Iteración #{i} ----"       
-      puts "Entrada: #{string[0,i+1]}"
-    
-      if string[0] =="#"
-        puts "Eliminar: #{string[0]}"
-        string.sub!(string[0], "")
-        puts "Salida: ''"
-      elsif string[i] == "#"
-        puts "Eliminar: #{string[i-1,2]}"
-        string.sub!(string[i-1,2], "")
-        i -= 2  
-        puts "Salida: #{string[0,i+1]}"
-      else
-        puts "Eliminar: Sin acción"   
-        puts "Salida: #{string[0,i+1]}"            
-      end
-        i+=1       
+  def initialize(cadena, arreglo, resultado)
+      @cadena = cadena
+      @arreglo = arreglo
+      @resultado = resultado
+  end
+
+  def nueva_cadena
+    @cadena.each_char do |caracter|
+      @arreglo.push(caracter) 
     end
-      puts "-------------------------"
-      puts "Salida final: '#{string}'"
+
+    @arreglo.each.with_index do |crctr, index|
+      @resultado = @resultado + crctr
+      if crctr == "#"
+        2.times do @resultado.chop! end 
+      end
+    end
+    puts @resultado
   end
 end
 
-obj_class = Proceso.new
-
-puts "Ingrese una cadena: "
-obj_class.analyze_string(string = gets.chomp)
-
+puts "Ingrese la cadena delimitada por: #"
+cadena = gets.chomp
+arreglo = []
+resultado = ""
+proceso = Proceso.new(cadena, arreglo, resultado)
+proceso.nueva_cadena
 
 
